f00f1ec0524cf5c46a4466a1c1bca89b71255d57
zirkledw <68195347+zirkledw@users.noreply.github.com>
2020-09-30 13:53:09 -0400

    Update pev2canvas.rb
    Add the ability for users to configure max score.

--- pev2canvas.rb
+++ pev2canvas.rb
@@ -1,9 +1,43 @@
 #  gem install smarter_csv
+#  file_name should be of the form:XXX_onewordmoduletitle_junknumbers.csv
 
-#  file_name should be of the form:onewordmoduletitle-threedigitclassnumber_gradebook
 require 'smarter_csv'
 require 'csv'
 Warning[:deprecated] = false
+
+def settings(file_name, module_name, course_number, max_score)
+
+  max_score = 0 if max_score.nil?
+
+  if file_name.nil?
+    puts 'You are missing a file name.  You need to at least give me a file name.'
+    exit
+  end
+
+  if module_name.nil? | course_number.nil?
+    puts 'Generating module and course number from the file name.'
+    file_name_parts = file_name.split(/\-|\_/)
+
+    course_number = file_name_parts[0]
+    module_name = file_name_parts[1]
+    puts "Generated module name: #{module_name.capitalize}"
+    puts "Generated course number: #{course_number}"
+    if max_score != 0
+      puts "Max score will be #{max_score}."
+    else
+      puts "Max score will be equal to the highest score."
+    end
+  end
+
+  puts "PEV results will be output to '#{module_name.capitalize}#{course_number}.csv'."
+  settings_hash = {
+    file_name: file_name,
+    course_number: course_number,
+    module_name: module_name,
+    max_score: max_score
+  }
+  return settings_hash
+end
 
 def input_grades(table)
   # inputs table of grade from pev as array of hash
@@ -41,30 +75,16 @@ class Array
   end
 end
 
-file_name, module_name, course_number = ARGV
+file_name, module_name, course_number, max_score = ARGV
 
-if file_name.nil?
-  puts 'You are missing a file name.  You need to at least give me a file name.'
-  exit
-end
+params = settings(file_name, module_name, course_number, max_score)
 
-if module_name.nil? | course_number.nil?
-  puts 'Generating module and course number from the file name.'
-  file_name_parts = file_name.split(/\-|\_/)
+pev_table = input_grades(params[:file_name])
 
-  course_number = file_name_parts[0]
-  module_name = file_name_parts[1]
-  puts "Generated module name: #{module_name.capitalize}"
-  puts "Generated course number: #{course_number}"
-end
+canvas_array = class_table(params[:course_number])
 
-puts "PEV results will be output to '#{module_name.capitalize}#{course_number}.csv'."
-pev_table = input_grades(file_name)
-
-canvas_array = class_table(course_number)
-
+max_score = params[:max_score]
 pev_results = {}
-max_score = 0
 
 pev_table.each do |x|
   grade = x['Total points earned'].to_i + x['Total answered'].to_i
@@ -74,12 +94,12 @@ end
 
 canvas_array.each do |x|
   if x[:student] == 'Points Possible'
-    x[module_name.downcase.to_sym] = max_score
+    x[params[:module_name].downcase.to_sym] = max_score
   else
-    x[module_name.downcase.to_sym] = pev_results[x[:email]]
+    x[params[:module_name].downcase.to_sym] = pev_results[x[:email]]
     x.delete(:email)
   end
 end
 
-output_file = module_name.capitalize + course_number + '.csv'
-canvas_array.to_csv(output_file, module_name)
+output_file = params[:module_name].capitalize + params[:course_number] + '.csv'
+canvas_array.to_csv(output_file, params[:module_name])
